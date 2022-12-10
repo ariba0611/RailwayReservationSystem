b7c9d68d632fac7e9a74ce099bc7ba6d560f7377
Ariba Siddiqui <aribasiddiqui1111@gmail.com>
2022-12-10 08:43:48 +0000

    Add project files.

---
+++ RailwayReservationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RailwayReservationSystem.Global" Language="C#" %>
---
+++ RailwayReservationSystem/Models/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RailwayReservationSystem.Models
+{
+    
+   
+        public class DataContext : DbContext
+        {
+           // public DbSet<Reservation> Reservations { get; set; }
+           
+        }
+    }
+
+
+
+
+
