202c20e6fbeb224562eaf5d28a0f1de0f0c9c506
Ariba Siddiqui <aribasiddiqui1111@gmail.com>
2022-12-15 07:43:12 +0000

    Reservation

--- RailwayReservationSystem/Controllers/HomeController.cs
+++ RailwayReservationSystem/Controllers/HomeController.cs
@@ -4,17 +4,66 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RailwayReservationSystem.Models;
+using RailwayReservationSystem.Models.DAL;
 
 namespace RailwayReservationSystem.Controllers
 {
     public class HomeController : Controller
     {
-        DataContext db = new DataContext();
-        // GET: Home
+
+        private IRailwayRepository<Reservation> interfaceObj;
+        public HomeController()
+        {
+            this.interfaceObj = new RailwayRepository<Reservation>();
+
+        }
+
         public ActionResult Index()
         {
-            var data = db.Reservations.ToList();
+            var data = interfaceObj.GetModel();
             return View(data);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Reservation res)
+
+        {
+
+            var data = interfaceObj.GetModel();
+            if (data.Count() >= 7)
+            {
+                TempData["msg"] = "<script>alert('YOU CANNOT ADD MORE THAN 6 PASSENGER');</script>";
+            }
+            else
+            {
+
+                interfaceObj.InsertModel(res);
+                interfaceObj.Save();
+
+                return RedirectToAction("Index");
+            }
+            return View(res);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Reservation rt = interfaceObj.GetModelByID(id);
+            return View(rt);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Reservation reservation)
+        {
+
+            interfaceObj.DeleteModel(id);
+            interfaceObj.Save();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
