ca312bfd87cb6e38e7d180f7846562fb62642016
Ariba Siddiqui <aribasiddiqui1111@gmail.com>
2022-12-15 07:11:29 +0000

    done

--- RailwayReservationSystem/Models/User.cs
+++ RailwayReservationSystem/Models/User.cs
@@ -16,8 +16,8 @@ namespace RailwayReservationSystem.Models
 
         public string LastName { get; set; }
         public string Gender { get; set; }
-        public string Phone { get; set; }
-
+        
+   
         public string Email { get; set; }
         public string Password { get; set; }
 
