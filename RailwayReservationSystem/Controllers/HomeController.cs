1a0a51feb0941cea809f5f6ce2d7b90daff4b48c
Ariba Siddiqui <aribasiddiqui1111@gmail.com>
2022-12-15 06:51:58 +0000

    Db

---
+++ RailwayReservationSystem/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RailwayReservationSystem.Models;
+
+namespace RailwayReservationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Reservations.ToList();
+            return View(data);
+        }
+    }
+}
--- RailwayReservationSystem/Models/DataContext.cs
+++ RailwayReservationSystem/Models/DataContext.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Sockets;
 using System.Web;
 
 namespace RailwayReservationSystem.Models
 {
     
-   
+  
         public class DataContext : DbContext
         {
-           // public DbSet<Reservation> Reservations { get; set; }
-           
-        }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<TrainDetails> TrainDetails { get; set; }
+
+    }
     }
 
 
